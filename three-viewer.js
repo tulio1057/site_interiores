22c8798a45f49953cbb81c340e1e860e14fa2dbc
tulio1057 <tuliojfb283@gmail.com>
2026-05-11 16:02:51 -0300

    Ajustes no carregamento dos modelos 3d

--- three-viewer.js
+++ three-viewer.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://unpkg.com/three@0.158.0/build/three.module.js';
-import { GLTFLoader }    from 'https://unpkg.com/three@0.158.0/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.158.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'three';
+import { GLTFLoader }    from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const canvas   = document.getElementById('threeCanvas');
 const wrap     = canvas.parentElement;
